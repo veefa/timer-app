7bd89ad0b2936dc3166f648c1be3e90e6ac6dcbd
vefa <44091688+veefa@users.noreply.github.com>
2025-04-01 23:15:24 +0000

    Initial of the project and cleaning up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css'
+
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center bg-gray-100 min-h-screen">
+      <h1 className="font-bold text-gray-800 text-2xl">Hello, TypeScript + React!</h1>
+    </div>
+  );
+};
+
+export default App;
