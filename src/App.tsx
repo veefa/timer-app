d0da9ea961f72c482f91cc312b97a45bdebca5ed
vefa <44091688+veefa@users.noreply.github.com>
2025-04-02 22:52:12 +0000

    creating and styling Timer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './index.css'
+import Timer from "./Timer";
 
 const App: React.FC = () => {
   return (
-    <div className="flex justify-center items-center bg-gray-100 min-h-screen">
-      <h1 className="font-bold text-gray-800 text-2xl">Hello, TypeScript + React!</h1>
+    <div className="">
+      <Timer/>
     </div>
   );
 };
---
+++ src/Timer.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+const Timer: React.FC = () => {
+    const [timeLeft, setTimeLeft] = useState<number>(25 * 60)   //Initializes timeLeft
+    const [isRunning, setIsRunning] = useState<boolean>(false);  // Tracks if the timer is running
+
+    // Starting and Stoping the timer then Cleaning up the interval
+    useEffect(() => {
+        let timer: ReturnType<typeof setInterval> | undefined;
+        if (isRunning && timeLeft > 0) {
+            timer = setInterval(() => {
+                setTimeLeft((prevTime) => prevTime - 1);
+            }, 1000);  //1000ms
+        } else {
+            clearInterval(timer);
+        }
+        //Preventing Multiple Intervals
+        return () => clearInterval(timer);}
+    , [isRunning, timeLeft]);
+
+    //Converts seconds into mm:ss format
+    const formatTime = (secconds: number) : string => {
+        const minutes = Math.floor(secconds / 60);
+        const secs = secconds % 60;
+        return `${String(minutes).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+    } 
+
+    return (
+        <div className="flex flex-col justify-center items-center space-y-4">
+            <h1 className="py-8 font-bold text-gray-600 text-5xl trex">{formatTime(timeLeft)}</h1>
+            <div className="space-x-4">
+                <button onClick={() => setIsRunning(!isRunning)}
+                    className="bg-blue-400 px-6 py-3 rounded-lg text-gray-50">
+                    {isRunning ? "Pause" : "Start"}
+                </button>
+                <button onClick={() => {
+                    setIsRunning(false);
+                    setTimeLeft(25 * 60);
+                    }}
+                    className="bg-red-700 px-5 py-3 rounded-lg text-white"
+                >
+                    Reset
+                </button>
+            </div>
+        </div>
+
+
+)
+
+};
+export default Timer;
